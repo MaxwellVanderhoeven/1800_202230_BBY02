e2364bd3acc962c07f0244c0462cdef84a75f711
michelle-0 <michelle.hung.12@gmail.com>
2022-11-02 10:14:57 -0700

    implemented navbar js

---
+++ scripts/skeleton.js
@@ -0,0 +1,6 @@
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./nav.html'));
+}
+loadSkeleton();  //invoke the function
+
+// if authenticated, use navebar that has profile button. If else, use navbar without profile button. 
